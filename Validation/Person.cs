6064cfe8ffba673c3b43ecd5df1a4c82b2c107b4
KlaasHagge <klaash@ppedv.de>
2022-01-13 12:23:22 +0100

    Modul 09: Validation (kommentiert)

--- Validation/Person.cs
+++ Validation/Person.cs
@@ -7,27 +7,35 @@ using System.Threading.Tasks;
 
 namespace Validation
 {
+    //Für ValidatesOnDataErrors muss z.B. das Interface IDataErrorInfo implementiert werden. Dieses erfordert die Einbindung von zwei zusätzlichen
     public class Person : IDataErrorInfo
     {
         private string name;
         public string Name
         {
             get { return name; }
-            set 
+            set
             {
+                //Bei ValidatesOnException wird im Fehlerfall eine Exception geworfen, welche von der GUI aufgefangen
+                //wird und als Validierungsfehler interpretiert wird. Die Exception-Message ist der
+                //ErrorContent
                 if (value.All(x => Char.IsLetter(x)))
                     name = value;
                 else
-                    throw new Exception("Bitte gib nur Buchstaben ein");
+                    throw new Exception("Bitte gib nur Buchstaben ein.");
             }
         }
-
 
         public int Alter { get; set; }
 
 
+        //Von IDataErrorInfo geforderte Properties
+
+        //Error wird von WPF nicht benutzt
         public string Error => null;
 
+        //Diese Property wird von der GUI als Validierung verwendet. Wenn ein nicht-leerer String zurückgegeben wird, 
+        //dann wird dies als Fehler interpretiert und dieser String als Fehlermeldung
         public string this[string columnName]
         {
             get
@@ -35,8 +43,7 @@ namespace Validation
                 switch (columnName)
                 {
                     case (nameof(Alter)):
-                        if (Alter < 0) return "Dein Alter ist kleiner als Null. FALSCH";
-                        if (Alter > 150) return "Du bist älter als es möglich ist";
+                        if (Alter < 0 || Alter > 150) return "Bitte gib dein wahres Alter an.";
                         break;
                     default:
                         break;
