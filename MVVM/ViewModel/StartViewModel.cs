6a0b818a73afe56a856d7c81d6a298e4f279c147
KlaasHagge <klaash@ppedv.de>
2022-01-14 14:13:19 +0100

    MVVM

---
+++ MVVM/ViewModel/StartViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM.ViewModel
+{
+    public class StartViewModel : INotifyPropertyChanged
+    {
+        public CustomCommand LadeDbCmd { get; set; }
+        public CustomCommand OeffeneDbCmd { get; set; }
+
+        public int AnzahlPersonen { get { return Model.Person.Personenliste.Count; } }
+
+        public StartViewModel()
+        {
+            LadeDbCmd = new CustomCommand
+                (
+                    p =>
+                    {
+                        Model.Person.LadePersonenAusDb();
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnzahlPersonen)));
+                    },
+                    p => AnzahlPersonen == 0
+                );
+
+            OeffeneDbCmd = new CustomCommand
+                (
+                    p =>
+                    {
+                        //Neues Fenster
+
+                        (p as Window).Close();
+                    },
+                    p => AnzahlPersonen > 0
+                );
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
